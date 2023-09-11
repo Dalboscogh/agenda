8e01c592339c56f8f4faa17777168f3b6046b69f
pamellalovatto <pamella.o.lovatto@gmail.com>
2023-09-11 18:55:41 -0300

    Configuração inicial: Header, Appbody e Footer 5

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 export default function Home () {
     return(
-        <Header />
-        <Appbody/>
-        <Footer />
+        <div>
+            <Header />
+            <Appbody />
+            <Footer />
+        </div>
+        
     );
 };
 
