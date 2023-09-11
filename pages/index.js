f224fa4e75fc0a7816dd791e81641b2bc90a1841
pamellalovatto <pamella.o.lovatto@gmail.com>
2023-09-11 17:56:28 -0300

    Teste Boiolisse pro meu amor

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Home () {
     return(
-        <h1>Teste da nossa agenda :D</h1>
+        <h1>Te amo demais, meu amor! Obrigada por ser você! Tenho muita sorte por ter o melhor professor do mundo s2</h1>
 
     );
 };
