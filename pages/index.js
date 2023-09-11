bb5256d785dfa6f152a0b3c845cb8c96bde2dc95
pamellalovatto <pamella.o.lovatto@gmail.com>
2023-09-11 19:00:45 -0300

    Ajustes no Header

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home () {
 
 function Header () {
     return(
-        <div>
+        <div className=''>
             <p>Agenda</p>
             <p>Profissionais</p>
             <p>Clientes</p>
@@ -23,6 +23,22 @@ function Header () {
         
     );
 };
+
+const Header = () => {
+    return (
+      <header className="flex justify-between items-center py-4 px-4 bg-gray-800">
+        <h1 className="text-white">My App</h1>
+        <nav className="flex items-center">
+          <a href="/" className="text-white">Home</a>
+          <a href="/agenda" className="text-white">Agenda</a>
+          <a href="/profissionais" className="text-white">Profissionais</a>
+          <a href="/clientes" className="text-white">Clientes</a>
+          <a href="/servicos" className="text-white">Serviços</a>
+          <a href="/relatorios" className="text-white">Relatórios</a>
+        </nav>
+      </header>
+    );
+};  
 
 function Appbody () {
 
