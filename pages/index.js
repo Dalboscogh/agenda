cbd5ac8e60c0e237a4967365552899a47c81b5df
Dalbosco <76927571+Dalboscogh@users.noreply.github.com>
2023-09-12 19:27:51 +0000

    ajustes, adição e configuração de editorconfig, prettier, tailwind e postcss ao projeto

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 
-export default function Home () {
-    return(
-        <Header />
-        <Appbody name="Picapau" />
-        <Footer />
-    );
-};
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Appbody />
+      <Footer />
+    </div>
+  );
+}
 
-function Header () {
-    return(
-        <p>Agenda</p>
-        <p>Profissionais</p>
-        <p>Clientes</p>
-        <p>Serviços</p>
-        <p>Relatórios</p>
-    );
-};
+function Header() {
+  return (
+    <div>
+      <p>Agenda</p>
+      <p>Profissionais</p>
+      <p>Clientes</p>
+      <p>Serviços</p>
+      <p>Relatórios</p>
+    </div>
+  );
+}
 
-function Appbody () {
-    return(
-        <h1>Essa é a sua agenda descomplicada para o dia a dia!</h1>
-    );
-};
+function Appbody() {
+  return <h1>Essa é a sua agenda descomplicada para o dia a dia!</h1>;
+}
 
-function Footer () {
-    return(
-        <p>Agenda</p>
-        <p>Profissionais</p>
-        <p>Clientes</p>
-        <p>Serviços</p>
-        <p>Relatórios</p>
-        <p>Acessibilidade</p>
-        <p>Ajuda</p>
-        <p>Fale conosco</p>
-    );
-};
+function Footer() {
+  return (
+    <div>
+      <p>Agenda</p>
+      <p>Profissionais</p>
+      <p>Clientes</p>
+      <p>Serviços</p>
+      <p>Relatórios</p>
+      <p>Acessibilidade</p>
+      <p>Ajuda</p>
+      <p>Fale conosco</p>
+    </div>
+  );
+}
